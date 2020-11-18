98b7d55814331ca706398feca293e2f0dcf9039a
Lacatux <jaimelacabexgonzalo@gmailc.om>
2020-11-18 12:27:12 +0100

    Ejercicio 2-1

--- Ejercicios 2 Java/src/EjercicioUno.java
+++ Ejercicios 2 Java/src/EjercicioUno.java
@@ -1,9 +1,35 @@
+import java.util.Scanner;
 
 public class EjercicioUno {
 
+	@SuppressWarnings("unused")
 	public static void main(String[] args) {
 
+		Scanner sc = new Scanner(System.in);
+		int[] numeros = new int[3];
+		int aux = 0;
+
+		for (int i = 0; i < numeros.length; i++) {
+			System.out.print("Introduce un número: ");
+			numeros[i] = sc.nextInt();
+		}
 		
+		do {
+			if (numeros[0] > numeros[1]) {
+				aux = numeros[1];
+				numeros[1] = numeros[0];
+				numeros[0] = aux;
+			} else if (numeros[1] > numeros[2]) {
+				aux = numeros[2];
+				numeros[2] = numeros[1];
+				numeros[1] = aux;
+			}
+		} while (numeros[0] > numeros[1] || numeros[1] > numeros[2]);
+		
+		System.out.println();
+		for (int i = 0; i < numeros.length; i++) {
+			System.out.print(numeros[i] + " ");
+		}
 
 	}
 
